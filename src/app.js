d7a3d474090f85e29e0291f85604661e34aec2c0
preeti453 <yadavpreeti30748@gmail.com>
2026-03-06 19:26:05 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ const authRouter = require("./routes/auth.routes")
 const accountRouter = require("./routes/account.routes")
 const transactionRouter = require("./routes/transaction.routes")
 
+app.get("/",(req,res)=>{
+    res.send("Ledger services is up and running.")
+})
 
 /**
  * User Routes
